095557e8b38d5da8353112eb3fdec9b3f085ab79
linghuam <1551827898@qq.com>
2018-10-16 09:54:35 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-    "extends": "standard",
-    "globals": {
-        "L": false,
-        "window": false
+    root: true,
+    extends: 'standard',
+    env: {
+        browser: true
+    },
+    globals: {
+        'L': false,
+        'window': false
     }
 };
